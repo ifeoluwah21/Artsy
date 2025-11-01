660fcbcde6983989c24e2a4c84895698796f7898
Ifeoluwah <spencerife.21@gmail.com>
2025-11-01 23:03:21 +0100

    feat: Completes the home page UI without functationalities
    - Add responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { HiArrowLongRight } from "react-icons/hi2";
 import "./App.css";
 import Carousel from "./components/Carousel";
 import Featured from "./components/Featured";
 import Header from "./components/Header";
+import Upcoming from "./components/Upcoming";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +12,24 @@ function App() {
       <Header />
       <Carousel />
       <Featured />
+      <Upcoming />
+      <div>
+        <ul className="py-24 text-2xl">
+          <li className="flex items-center border-t border-b px-8 py-6">
+            Explore marketplace
+            <button className="ml-auto rounded-full border p-2">
+              <HiArrowLongRight className="h-10 w-10" />
+            </button>
+          </li>
+          <li className="flex items-center border-b px-8 py-6">
+            See auctions
+            <button className="ml-auto rounded-full border p-2">
+              <HiArrowLongRight className="h-10 w-10" />
+            </button>
+          </li>
+        </ul>
+      </div>
+      <Footer />
     </>
   );
 }
